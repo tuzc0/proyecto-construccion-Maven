71a98f5e94d6686940747f81ffb26fbeca61cc13
tuzc0 <claudiojtz1402@gmail.com>
2025-04-21 15:02:24 -0600

    Se agrego javaFX

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main {
+public class Main extends Application {
     private static final Logger logger = LogManager.getLogger();
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        logger.info("Iniciando la aplicación JavaFX...");
+
+        // Carga el archivo FXML
+        Parent root = FXMLLoader.load(getClass().getResource("/interfaz.fxml"));
+
+        // Configura la ventana principal
+        primaryStage.setTitle("Aplicación JavaFX con Scene Builder");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        logger.info("Aplicación JavaFX iniciada correctamente.");
+    }
+
     public static void main(String[] args) {
-        logger.info("Esto es un mensaje informativo.");
-        logger.debug("Mensaje de depuración.");
-        logger.error("¡Algo salió mal!");
+        logger.info("Lanzando la aplicación...");
+        launch(args);
     }
 }
